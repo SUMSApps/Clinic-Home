b9b6617eb4b41b17aa7e290c3c0d58a0b8f92180
SUMSApps <45171164+SUMSApps@users.noreply.github.com>
2021-01-08 12:42:56 +0000

    new

---
+++ story_content/user.js
@@ -0,0 +1,175 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jid1viq7J4":
+        Script1();
+        break;
+      case "6SLGhegJw37":
+        Script2();
+        break;
+      case "5hlfTIial8i":
+        Script3();
+        break;
+      case "6XY4uZSUaKa":
+        Script4();
+        break;
+      case "67eLHHPVlrH":
+        Script5();
+        break;
+      case "6kfs6WoQFFB":
+        Script6();
+        break;
+      case "5ZSJgMXHfnD":
+        Script7();
+        break;
+      case "6lWuNVapBkk":
+        Script8();
+        break;
+      case "6YDz8ufpzFi":
+        Script9();
+        break;
+      case "667pmQ76lER":
+        Script10();
+        break;
+      case "5YibdlDLVH5":
+        Script11();
+        break;
+      case "5XFc61tfCdT":
+        Script12();
+        break;
+      case "6mzVRKLCXKt":
+        Script13();
+        break;
+      case "5X0gACcSwFW":
+        Script14();
+        break;
+      case "65rXLFRbuEV":
+        Script15();
+        break;
+      case "62WGacZfw9z":
+        Script16();
+        break;
+      case "6Cf34PaxVsT":
+        Script17();
+        break;
+      case "6NlvgloBkzA":
+        Script18();
+        break;
+      case "6P4fO3zksDv":
+        Script19();
+        break;
+      case "5uWg5Q4LRRM":
+        Script20();
+        break;
+      case "5X2ILXKfZyd":
+        Script21();
+        break;
+  }
+}
+
+function Script1()
+{
+  ga('send', 'screenview', {screenName: 'WhatIsLipodystrophy'});
+}
+
+function Script2()
+{
+  ga('send', 'screenview', {screenName: 'CommonCausesSideEffectsAndExamples'});
+}
+
+function Script3()
+{
+  ga('send', 'screenview', {screenName: 'CommonCauses'});
+}
+
+function Script4()
+{
+  ga('send', 'screenview', {screenName: 'SideEffectsOfInjectingIntoALipo'});
+}
+
+function Script5()
+{
+  ga('send', 'screenview', {screenName: 'Examples'});
+}
+
+function Script6()
+{
+  ga('send', 'screenview', {screenName: 'HowToSelfExamine'});
+}
+
+function Script7()
+{
+  ga('send', 'screenview', {screenName: 'WhatToDoIfYouFindLipodystrophy'});
+}
+
+function Script8()
+{
+  ga('send', 'screenview', {screenName: 'HowToAvoidLipodystrophy'});
+}
+
+function Script9()
+{
+  ga('send', 'screenview', {screenName: 'BraunRotatingInjectionSites'});
+}
+
+function Script10()
+{
+  ga('send', 'screenview', {screenName: 'BDTheImportanceOfInjectionSiteRotation'});
+}
+
+function Script11()
+{
+  ga('send', 'screenview', {screenName: 'TRENDUKGuidelines'});
+}
+
+function Script12()
+{
+  ga('send', 'screenview', {screenName: 'HowToGetItRight'});
+}
+
+function Script13()
+{
+  ga('send', 'screenview', {screenName: 'Correct10StepInjectionProcess'});
+}
+
+function Script14()
+{
+  ga('send', 'screenview', {screenName: 'ScientificEvidence'});
+}
+
+function Script15()
+{
+  ga('send', 'screenview', {screenName: 'Credits'});
+}
+
+function Script16()
+{
+  ga('send', 'screenview', {screenName: 'ShareThisResource'});
+}
+
+function Script17()
+{
+  ga('send', 'event', 'Button', 'click', 'Twitter');
+}
+
+function Script18()
+{
+  ga('send', 'event', 'Button', 'click', 'Facebook');
+}
+
+function Script19()
+{
+  ga('send', 'screenview', {screenName: 'DownloadAsAnApp'});
+}
+
+function Script20()
+{
+  ga('send', 'event', 'Button', 'click', 'Android');
+}
+
+function Script21()
+{
+  ga('send', 'event', 'Button', 'click', 'Apple');
+}
+
